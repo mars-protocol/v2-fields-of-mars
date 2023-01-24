56647aec9ad0e4bdef6873a6590184e1502154f4
Gabe Rodriguez <grod220@gmail.com>
2023-01-24 18:21:41 +0100

    Update testnet credit line granting

--- scripts/types/config.ts
+++ scripts/types/config.ts
@@ -24,7 +24,7 @@ export interface DeploymentConfig {
   oracle: { addr: string }
   redBank: { addr: string }
   vaults: VaultInstantiateConfig[]
-  allowedCoins: { denom: string; priceSource: PriceSource }[]
+  allowedCoins: { denom: string; priceSource: PriceSource; grantCreditLine: boolean }[]
   maxCloseFactor: string
   maxValueForBurn: string
   maxUnlockingPositions: string
